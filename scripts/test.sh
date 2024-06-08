d94cfeb9d973996124d6e8da5cd3f737c2757fa5
Chassagnol <rvc1@pn115038.campus.nist.gov>
2024-06-08 18:37:50 -0400

    added threads configuration in the test script todo: udpate parse results

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-MATRIX_FILES_DIR=../../test-generator/scripts/cholesky/
+# MATRIX_FILES_DIR=../../test-generator/scripts/cholesky/
+MATRIX_FILES_DIR=../../test-generator/build/cholesky/
 CHOLESKY=../cholesky
 RESULT_OUTPUT_DIR=./result
+
+THREADS_MIN=10
+THREADS_MAX=40
+THREADS_INC=5
 
 mkdir -p $RESULT_OUTPUT_DIR
 
 for file in $(ls $MATRIX_FILES_DIR); do
   timesfile="times-$(echo $file | sed 's/\.in$/.txt/')"
-  for i in {1..10}; do
-    $CHOLESKY $MATRIX_FILES_DIR/$file lapack >> $RESULT_OUTPUT_DIR/$timesfile
+  for ((threads=THREADS_MIN; threads<=THREADS_MAX; threads+=THREADS_INC)); do
+    mkdir -p $RESULT_OUTPUT_DIR/$threads
+    for i in {1..10}; do
+      $CHOLESKY $MATRIX_FILES_DIR/$file lapack >> $RESULT_OUTPUT_DIR/$threads/$timesfile
+    done
   done
 done
